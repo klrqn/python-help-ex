95eb181f57d3afc81b4917d5d66840bd5f7428e1
john doe <scottbruceburton@gmail.com>
2020-08-06 22:35:11 -0400

    added exec and help(ex)

---
+++ python_help_ex.py
@@ -0,0 +1,36 @@
+def add(x, y):
+    """
+    This function adds the given integer arguments
+    :param x: integer
+    :param y: integer
+    :return: integer
+    """
+    return x + y
+
+class Employee:
+    """
+    Employee class, mapped to "employee" table in 
+    Databas
+    """
+    id = 0
+    name = ''
+
+    def __init__(self, i, n):
+        """
+        Employee object constructor
+        :param i: integer, must be positive
+        :param n: string
+        """
+        self.id = i
+        self.name = n
+
+# First execute the script to load the function
+# and class definition in py shell.
+# executing here creates a recursion error! ofc
+# exec(open("python_help_ex.py").read())
+
+# the practice viewing the help()
+help('python_help_ex')
+help('python_help_ex.add')
+help('python_help_ex.add')
+
